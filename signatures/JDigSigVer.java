bc6932ab0802510f5f55c4cdd42fad7e129e9d4d
Shashank J <shashankreddy231@gmail.com>
2021-02-01 17:40:08 +0530

    Generation of Javadoc for JDigSigVer.java class.

--- signatures/JDigSigVer.java
+++ signatures/JDigSigVer.java
@@ -3,9 +3,24 @@ package signatures;
 import java.nio.charset.StandardCharsets;
 import java.security.*;
 
+/**
+ * Class which demonstrates how to create and verify digital signature.
+ * @author shashank-shark
+ */
 public class JDigSigVer {
 
+    /**
+     * The steps in generating digital signature and verification include:
+     * <ol>
+     *     <li>Generation of DSA KeyPair.</li>
+     *     <li>Generate Signature using the DSA keypair that was generated in the above step.</li>
+     *     <li>Digitally sign a message.</li>
+     *     <li>Verify the signature.</li>
+     * </ol>
+     * @param args - command line arguments
+     */
     public static void main (String[] args) {
+
         KeyPair dsaKeyPair = generateDSAKeyPair();
         Signature dsaSignature = getSHA256WithDSASignature(dsaKeyPair);
 
@@ -14,7 +29,7 @@ public class JDigSigVer {
          */
         byte[] message = "qwertyuiopasdfghjklzxvbnm".getBytes(StandardCharsets.UTF_8);
         byte[] signedMessage = signMessageWithSignature(message, dsaSignature);
-        boolean isVerificationSuccessful = validateDigitalSignature(dsaKeyPair,message, signedMessage);
+        boolean isVerificationSuccessful = validateDigitalSignature(dsaKeyPair.getPublic(), message, signedMessage);
         System.out.println("Positive Scenario : " + isVerificationSuccessful);
 
         /* CASE: Negative Signature Verification
@@ -22,19 +37,30 @@ public class JDigSigVer {
          */
         byte[] newMessage = "qwertyuiopasdfghjklmnbvcxz".getBytes(StandardCharsets.UTF_8);
         signedMessage = signMessageWithSignature(newMessage, dsaSignature);
-        isVerificationSuccessful = validateDigitalSignature(dsaKeyPair,message, signedMessage);
+        isVerificationSuccessful = validateDigitalSignature(dsaKeyPair.getPublic(),message, signedMessage);
         System.out.println("Negative Scenario : " + isVerificationSuccessful);
     }
 
-    private static boolean validateDigitalSignature(KeyPair keyPair, byte[] message, byte[] digitalSignature) {
-        Signature signature = getSHA256WithDSASignature(keyPair);
+    /**
+     * method to check whether the message recieved is the same message that was sent from the sender.
+     * @param publicKey - public key of the sender
+     * @param message - message recieved
+     * @param digitalSignature - the digital signature of this particular message
+     * @return boolean - true on successful signature verification and false on unsuccessful verification
+     * verification is not successful
+     */
+    public static boolean validateDigitalSignature(PublicKey publicKey, byte[] message, byte[] digitalSignature) {
+
+        Signature signature = null;
         try {
-            signature.initVerify(keyPair.getPublic());
-        } catch (InvalidKeyException e) {
+            signature = Signature.getInstance("SHA256WithDSA");
+            signature.initVerify(publicKey);
+        } catch (InvalidKeyException | NoSuchAlgorithmException e) {
             e.printStackTrace();
         }
 
         try {
+            assert signature != null;
             signature.update(message);
         } catch (SignatureException e) {
             e.printStackTrace();
@@ -47,7 +73,11 @@ public class JDigSigVer {
         return false;
     }
 
-    private static KeyPair generateDSAKeyPair() {
+    /**
+     * method for generatin DSA Keypair.
+     * @return KeyPair
+     */
+    public static KeyPair generateDSAKeyPair() {
         KeyPair keyPair = null;
         try {
             KeyPairGenerator keyPairGenerator = KeyPairGenerator.getInstance("DSA");
@@ -59,7 +89,11 @@ public class JDigSigVer {
         return keyPair;
     }
 
-    private static KeyPair generateRSAKeyPair() {
+    /**
+     * method for generating RSA Keypair.
+     * @return KeyPair
+     */
+    public static KeyPair generateRSAKeyPair() {
         KeyPair keyPair = null;
         try {
             KeyPairGenerator keyPairGenerator = KeyPairGenerator.getInstance("RSA");
@@ -71,7 +105,13 @@ public class JDigSigVer {
         return keyPair;
     }
 
-    private static byte[] signMessageWithSignature(byte[] message, Signature signature) {
+    /**
+     * method to create digital signature of the message.
+     * @param message - the message that should be digitally signed
+     * @param signature - DSA signature instance.
+     * @return byte[] - array stream of digitally signed bytes.
+     */
+    public static byte[] signMessageWithSignature(byte[] message, Signature signature) {
 
         try {
             signature.update(message);
@@ -86,7 +126,12 @@ public class JDigSigVer {
         return null;
     }
 
-    private static Signature getSHA256WithDSASignature(KeyPair keyPair) {
+    /**
+     * method to generate Signature instance from the given KeyPair instance.
+     * @param keyPair - the generated DSA KeyPair.
+     * @return Signature - signature instance
+     */
+    public static Signature getSHA256WithDSASignature(KeyPair keyPair) {
         Signature signature = null;
         try {
             signature = Signature.getInstance("SHA256WithDSA");
